da16aa4e8ccb1ccdf6f60d8d1a1debc431a0e4f7
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-10-14 11:52:57 +0700

    Log clean up

--- src/claire/core.cljs
+++ src/claire/core.cljs
@@ -79,14 +79,12 @@
          (clj->js args))
        "\n"))
 
-(defn ^{:cmd "claire.run"} launch [*sys]
+(defn ^{:cmd "claire.run"} run [*sys]
   (p/promise [resolve _]
     (let [run-configuration-keys (keys (get @*sys :claire/run-configuration))]
       (p/let [run-configuration-key (gui/show-quick-pick run-configuration-keys {:placeHolder "Run..."})]
         (when-let [{:keys [run args] :or {run :clojure}} (get-in @*sys [:claire/run-configuration run-configuration-key])]
-          (let [^js output-channel (get @*sys :claire/output-channel)
-
-                command (case run
+          (let [command (case run
                           :clojure "clojure"
                           :lein "lein"
                           ;; `clojure` is the default command.
@@ -96,32 +94,33 @@
 
                 cwd (or (root-path) (os/tmpdir))
 
-                _ (.appendLine output-channel "Lauching program, please wait...\n")
-                _ (.appendLine output-channel (log-str-program command args))
-                _ (.appendLine output-channel (log-str-cwd cwd))
-                _ (.show output-channel true)
+                _ (-> (out *sys)
+                      (log "Lauching program, please wait...\n"
+                           (log-str-program command args)
+                           (log-str-cwd cwd))
+                      (show-log))
 
                 process (child-process/spawn command (clj->js args) #js {:cwd cwd})
 
                 _ (.on (.-stdout process) "data"
                        (fn [data]
-                         (let [output-channel (get @*sys :claire/output-channel)]
-                           (.appendLine output-channel data)
-                           (.show output-channel true))))
+                         (-> (out *sys)
+                             (log data)
+                             (show-log))))
 
                 _ (.on (.-stderr process) "data"
                        (fn [data]
-                         (let [output-channel (get @*sys :claire/output-channel)]
-                           (.appendLine output-channel data)
-                           (.show output-channel true))))
+                         (-> (out *sys)
+                             (log data)
+                             (show-log))))
 
                 _ (.on process "close"
                        (fn [code]
-                         (let [output-channel (get @*sys :claire/output-channel)]
-                           (swap! *sys dissoc :claire/program)
+                         (swap! *sys dissoc :claire/program)
 
-                           (.appendLine output-channel (str "\nProgram exited with code " code ".\n"))
-                           (.show output-channel true))))]
+                         (-> (out *sys)
+                             (log (str "\nProgram exited with code " code ".\n"))
+                             (show-log))))]
 
             (swap! *sys assoc :claire/program {:claire.program/command command
                                                :claire.program/args args
@@ -130,17 +129,16 @@
 
             (resolve nil)))))))
 
-(defn ^{:cmd "claire.stop"} kill [*sys]
+(defn ^{:cmd "claire.stop"} stop [*sys]
   (when-let [^js process (get-in @*sys [:claire/program :claire.program/process])]
     (-> (out *sys)
         (log "\nStopping program..."))
 
     (.kill process)))
 
-(defn ^{:cmd "claire.evalSelection"} send-selection-to-repl [*sys editor _ _]
+(defn ^{:cmd "claire.evalSelection"} eval-selection [*sys editor _ _]
   (if-let [^js process (get-in @*sys [:claire/program :claire.program/process])]
-    (let [^js output-channel (get @*sys :claire/output-channel)
-          ^js document (oget editor "document")
+    (let [^js document (oget editor "document")
           ^js selection (oget editor "selection")
           ^js range (vscode/Range. (oget selection "start") (oget selection "end"))
 
@@ -160,13 +158,10 @@
     (.clear output-channel)))
 
 (defn ^{:cmd "claire.info"} info [*sys]
-  (let [^js output-channel (get @*sys :claire/output-channel)
-
-        {:keys [:claire.program/command
+  (let [{:keys [:claire.program/command
                 :claire.program/args
                 :claire.program/cwd
                 :claire.program/process]} (get @*sys :claire/program)]
-
     (if process
       (-> (out *sys)
           (log (log-str-program command args)
@@ -219,16 +214,17 @@
         output-channel (vscode/window.createOutputChannel "Claire")]
 
     (with-dispose context
-      (register-command *sys #'launch)
-      (register-command *sys #'kill)
-      (register-text-editor-command *sys #'send-selection-to-repl)
+      (register-command *sys #'run)
+      (register-command *sys #'stop)
+      (register-text-editor-command *sys #'eval-selection)
       (register-text-editor-command *sys #'clear-output)
       (register-text-editor-command *sys #'info))
 
     (reset! *sys {:claire/output-channel output-channel
                   :claire/run-configuration (merge config default-config)})
 
-    (.appendLine output-channel "Claire is active.\n"))
+    (-> (out *sys)
+        (log "Claire is active.\n")))
 
   nil)
 
